2cbef57b2721bb0d092af55e76223172df9c56f5
Kwanghoon Son <kwangson@yahoo.com>
2025-12-31 21:42:06 +0900

    Sidebar

---
+++ app/accounts/me/page.tsx
@@ -0,0 +1,8 @@
+export default function MyProfilePage() {
+  return (
+    <div>
+      <h1>My Profile Page</h1>
+      <p>This is your personal profile page.</p>
+    </div>
+  );
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,40 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { MessageSquare, User } from "lucide-react";
+import Link from "next/link";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <Link href="/m" passHref>
+              <SidebarMenuButton>
+                <MessageSquare className="h-5 w-5" />
+                Messages
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <Link href="/accounts/me" passHref>
+              <SidebarMenuButton>
+                <User className="h-5 w-5" />
+                Profile
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
